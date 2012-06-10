c8581b32f09bc39c02688919b2fc07730b5f5174
Reinholds Zviedris <contact@digiblink.eu>
2012-06-10 18:59:42 +0300

    Updated SA core + added Twig
    Added Twig Templating engine - http://twig.sensiolabs.org/

--- sa/index.php
+++ sa/index.php
@@ -1,16 +1,41 @@
 <?php
 
-	define('SA_BASE', $_SERVER["REQUEST_URI"]);
+	require_once('./config.php');
+
+	if(!defined('SA_BASE')) {
+		echo 'Admin not configured!';
+		exit();
+	}
 	
 	require_once('gluephp/glue.php');
 	
 	$urls = array(
-		SA_BASE => 'index'
+		SA_BASE => 'index',
+		SA_BASE.'login' => 'login'
 	);
+
+	global $is_logged_in;
+	$is_logged_in = false;
 
 	class index {
 		function GET() {
-			echo "Hello, World!";
+			global $is_Logged_in;
+			if($is_logged_in) {
+				echo "Hello, World!";
+			} else {
+				header('Location: '.SA_BASE.'login/');
+				exit();
+			}
+		}
+	}
+	
+	class login {
+		function GET() {
+			echo 'Login!';
+		}
+		function POST() {
+			global $is_Logged_in;
+			$is_logged_in = true;
 		}
 	}
 
